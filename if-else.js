24866a213d32f0128a7853f01162927d3268a6f0
Marcell Dactes <83461178+marcelldac@users.noreply.github.com>
2022-04-19 18:03:49 -0300

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,16 @@
+// código 
+semaforo = "amarelo"; //declaração de variável
+
+if (semaforo == "verde"){ //condicional if (se o semaforo for verde imprima 'pode passar')
+
+    console.log("Pode passar!");
+
+} else if(semaforo == "vermelho") { //condicional else if (se o semaforo for vermelho imprima 'parar')
+
+    console.log("Parar");
+
+} else { //condicional else (se o sinal não for verde, nem vermelho, imprima 'atenção')
+
+    console.log("Atenção!");
+}
+// código
